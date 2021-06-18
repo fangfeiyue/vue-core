18dafdd796849dddc188bb9f38653a827a6149d4
FangFeiyue <18766131540@163.com>
2021-06-18 11:50:26 +0800

    将对象递归设置成响应式的

--- src/observer/index.js
+++ src/observer/index.js
@@ -13,6 +13,21 @@ class Observer {
 
 // 没有放到原型上的原因是因为这个方法不一定是通过实例调用的，如Vue.util.defineReactive
 export function defineReactive(data, key, value) {
+  // value有可能也是一个对象，需要递归遍历，所以vue2中数据尽量不要嵌套过深，会浪费性能
+  /* 
+  data() {
+        return {
+          name: 'f',
+          obj: {
+            name: 1,
+            age: 2
+          }
+        }
+      }
+  */
+  // Object.defineProperty只是重写了对象的get和set，Proxy是给对象设置代理不用改写对象性能高些，两者不一样
+  observe(value)
+
   Object.defineProperty(data, key, {
     get() {
       return value
@@ -20,6 +35,11 @@ export function defineReactive(data, key, value) {
     set(newValue) {
       // 如果新值和旧值相等，则不做任何操作
       if (newValue ===  value) return
+      /* 
+      如果用户重重新给数据赋值成新值且这个新值是对象，要将这个对象设置成响应式的，如：
+      vm.obj = {name:'d'}
+      */
+      observe(newValue)
       value = newValue
     }
   })
