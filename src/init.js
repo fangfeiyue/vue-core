4394819e70fd03560893187d5b0577bd6116149b
FangFeiyue <18766131540@163.com>
2021-06-18 16:30:07 +0800

    实现模板编译

--- src/init.js
+++ src/init.js
@@ -1,4 +1,4 @@
-import { initState } from "./state";
+import { initState } from './state';
 
 export function initMixin(Vue) {
 	Vue.prototype._init = function(options) {
@@ -7,7 +7,28 @@ export function initMixin(Vue) {
 
 		// 初始化状态
 		initState(vm);
+
+		// 有el属性的话，说明数据可以挂在到页面上
+		if (vm.$options.el) {
+			vm.$mount(vm.$options.el);
+		}
+	};
+	Vue.prototype.$mount = function(el) {
+		const vm = this;
+		const options = vm.$options;
+		el = document.querySelector(el);
+
+    // 如果有render就直接用render，没有render，看看有没有template属性，如果也没有template属性的话，就直接找外部模板
+		// 如果没有render方法
+		if (!options.render) {
+			let template = options.template;
+			// 如果没有模板但是有el
+			if (!template && el) {
+				template = el.outerHTML;
+			}
+      // 将模板编译成render函数
+			const render = compileToFunctions(template);
+			options.render = render;
+		}
 	};
 }
-
-
