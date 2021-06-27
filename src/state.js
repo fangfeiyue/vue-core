9babbe2c987230a7cccdf325f9887e4a18ce707d
FangFeiyue <18766131540@163.com>
2021-06-27 09:58:18 +0800

    no message

--- src/state.js
+++ src/state.js
@@ -34,7 +34,7 @@ function proxy(vm, source, key) {
 function initData(vm) {
 	let data = vm.$options.data;
 	// 对data类型进行判断，如果是函数获取函数返回值作为对象，使用call是保证当前的this指向实例，因为我们可以在data中调用this.xxx
-	data = vm._data = vm._data = typeof data === 'function' ? data.call(vm) : data;
+	data = vm._data = typeof data === 'function' ? data.call(vm) : data;
 	// 通过 vm._data获取劫持后的数据，用户就可以通过拿到_data了，要不虽然我们对数据进行了劫持，但是用户无法拿到劫持后的data，因为如果data是一个函数，函数执行完返回的对象外部无法拿到
 
 	// 将_data中的数据全部放到vm上，让用户可以通过vm.xx直接拿到数据，而不用像vm._data.xxx这么麻烦
