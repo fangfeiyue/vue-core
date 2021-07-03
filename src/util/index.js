b30b6b5d5d10072b0d1dd0957d8b1ce43702ddd6
FangFeiyue <18766131540@163.com>
2021-07-03 10:57:03 +0800

    生命周期

--- src/util/index.js
+++ src/util/index.js
@@ -8,33 +8,39 @@ export const LIFECYCLE_HOOKS = [
 	'beforeDestroy',
 	'destroyed'
 ];
-export const isObject = (value) => typeof value === 'object' && value !== null
+export const isObject = (value) => typeof value === 'object' && value !== null;
 const strats = {};
+
+// 核心就是把生命周期钩子函数变成一个数组
 function mergeHook(parentVal, childValue) {
 	if (childValue) {
 		if (parentVal) {
+      
 			return parentVal.concat(childValue);
 		} else {
+      // 儿子有父亲没有
 			return [ childValue ];
 		}
 	} else {
+    // 如果儿子没有就用父亲的
 		return parentVal;
 	}
 }
+// 把这些钩子都放到策略上
 LIFECYCLE_HOOKS.forEach((hook) => {
 	strats[hook] = mergeHook;
 });
 strats.components = function(parentVal, childVal) {
-  const res = Object.create(parentVal);
-  if (childVal) {
-    for (let key in childVal) {
-      res[key] = childVal[key];
-    }
-  }
-  return res;
-}
+	const res = Object.create(parentVal);
+	if (childVal) {
+		for (let key in childVal) {
+			res[key] = childVal[key];
+		}
+	}
+	return res;
+};
 export function mergeOptions(parent, child) {
-  /* 
+	/* 
   合并策略：
   如果父亲有的儿子也有，应该用儿子替换父亲， 如 父元素的数据是{a:1} 子元素的数据是{a:2}合并后，应该是 {a:2}
 
@@ -46,21 +52,38 @@ export function mergeOptions(parent, child) {
 		mergeField(key);
 	}
 	for (let key in child) {
+		// 父亲没有儿子有
 		if (!parent.hasOwnProperty(key)) {
 			mergeField(key);
 		}
 	}
 	function mergeField(key) {
+		// if (strats[key]) {
+		// 	options[key] = strats[key](parent[key], child[key]);
+		// } else {
+		// 	if (typeof parent[key] == 'object' && typeof child[key] == 'object') {
+		// 		options[key] = {
+		// 			...parent[key],
+		// 			...child[key]
+		// 		};
+		// 	} else {
+		// 		options[key] = child[key];
+		// 	}
+		// }
+
+		// 策略模式
 		if (strats[key]) {
-			options[key] = strats[key](parent[key], child[key]);
+			return options[key] = strats[key](parent[key], child[key]);
+		}
+
+		if (isObject(parent[key]) && isObject(child[key])) {
+			options[key] = { ...parent[key], ...child[key] };
 		} else {
-			if (typeof parent[key] == 'object' && typeof child[key] == 'object') {
-				options[key] = {
-					...parent[key],
-					...child[key]
-				};
-			} else {
+			if (child[key]) {
+				// 如果儿子有值
 				options[key] = child[key];
+			} else {
+				options[key] = parent[key];
 			}
 		}
 	}
