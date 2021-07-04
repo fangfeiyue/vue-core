ee1342977a0e39bdff978027433750a529a8ae9c
FangFeiyue <18766131540@163.com>
2021-07-04 19:20:51 +0800

    diff算法属性比对

--- src/observer/patch.js
+++ src/observer/patch.js
@@ -4,11 +4,13 @@ oldVnode 是一个真实节点
 export function patch(oldVnode, vnode) {
 	// 如果挂载的时候没有元素，直接创建元素
 	// 组件调用patch方法会产生$el属性，这个属性对应的就是真实元素
+  // 1. 组件
 	if (!oldVnode) {
 		return createElm(vnode); // 根据虚拟节点创建元素
 	}
 
 	// 是否是真实的 DOM 节点
+  // 2. 初次渲染
 	const isRealElement = oldVnode.nodeType;
 	if (isRealElement) {
 		// 如果oldVnode是一个真实DOM，表示是初次渲染
@@ -22,7 +24,24 @@ export function patch(oldVnode, vnode) {
 		parentElm.removeChild(oldVnode);
 		// 把新创建的 el 替换成 vm.$el
 		return el;
-	}
+	} else {
+    // 3. diff 算法的比对
+    // 1. 如果两个虚拟节点的标签不一样，那就直接替换，不用比对了，如div换成p
+    if (oldVnode.tag !== vnode.tag){
+      return oldVnode.el.parentNode.replaceChild(createElm(vnode), oldVnode.el)
+    }
+    // 2. 标签一样，但是是两个文本元素，文本元素的 tag 值是 undefined
+    if (!oldVnode.tag) {
+      if (oldVnode.text !== vnode.text) {
+        return oldVnode.el.textContent = vnode.text
+      }
+    }
+    // 3. 元素相同，复用老节点，并且更新属性
+    const el = vnode.el = oldVnode.el;
+    // 用老的属性和新的虚拟节点进行比对
+    updateProperties(vnode, oldVnode.data);
+
+  }
 }
 function createComponent(vnode) {
 	let i = vnode.data;
@@ -38,7 +57,7 @@ function createComponent(vnode) {
 }
 
 // 根据虚拟节点创建真实节点
-function createElm(vnode) {
+export function createElm(vnode) {
 	let { tag, children, key, data, text } = vnode;
 	// 如果是标签，有可能是组件，这里忽略
 	if (typeof tag === 'string') {
@@ -62,9 +81,30 @@ function createElm(vnode) {
 	return vnode.el;
 }
 // 更新节点属性
-function updateProperties(vnode) {
+function updateProperties(vnode, oldProps = {}) {
 	let newProps = vnode.data || {}; // 获取当前老节点中的属性
 	let el = vnode.el; // 当前的真实节点
+
+  // 老的属性新的没有，需要删除属性
+  for (let key in oldProps) {
+    if (!newProps[key]) {
+      el.removeAttribute(key);
+    }
+  }
+
+  /* 
+  旧的 <div style="color:red;">{{name}}</div>
+  新的 <div style="background:blue;">{{name}}</div>
+  */
+  const newStyle = newProps.style || {}
+  const oldStyle = oldProps.style || {}
+  for (let key in oldStyle) { // 判断样式
+    if (!newStyle[key]) {
+      el.style[key] = '';
+    }
+  }
+
+  // 新的属性老的没有，直接用新的覆盖，不考虑没有
 	for (let key in newProps) {
 		// 处理样式
 		if (key === 'style') {
